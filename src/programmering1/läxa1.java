47ca9598e911c971fff4730093ef52c68fb91e0c
mtafar <mtanus_messi@hotmail.com>
2018-09-07 15:06:34 +0200

    scanner complete

--- src/programmering1/läxa1.java
+++ src/programmering1/läxa1.java
@@ -1,5 +1,12 @@
 package programmering1;
-
+import java.util.Scanner;
 public class läxa1 {
-
+public static void main(String[] args) {
+	scanner input = new Scanner (system.in);
+	
+}
+	
+	
+	
+	
 }
