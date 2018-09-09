65c6dc10b2c0330d5b4e0c40f75036e5f20da9fb
mtafar <mtanus_messi@hotmail.com>
2018-09-09 18:03:32 +0200

    klar

--- src/programmering1/läxa1.java
+++ src/programmering1/läxa1.java
@@ -1,17 +1,32 @@
 package programmering1;
 import java.util.Scanner;
 public class läxa1 {
-public static void main(String[] args) {
-	Scanner input = new Scanner (system.in);
-	System.out.println("vad heter du");
-	input.next(); 
-System.out.println("hur gammal är du");
-input.next();
-System.out.println("");
+	public static void main(String[] args) {
+		Scanner input = new Scanner (System.in);
+		System.out.println("vad heter du");
+		String namn	= input.nextLine(); 
 
+		System.out.println("Hur gammal är du?");
+		String age = input.nextLine();
 
-
-
+		System.out.println("Vad är din adress?");
+		String adress = input.nextLine();
+	
+		System.out.println("Vad är ditt postnummer?");
+		String postnummer = input.nextLine();
+	
+		System.out.println("Vilken stad bor du i?");	
+		String stad = input.nextLine();
+		
+		System.out.println("Vad är ditt telefonnummer?");
+		String tele = input.nextLine(); 
+				
+		System.out.println("Information:");
+		System.out.println("Namn:    " +namn);
+		System.out.println("Ålder: 	 " +age+" år");
+		System.out.println("adress:  " +adress);
+		System.out.println("         "+postnummer+" "+stad);
+		System.out.println("telefon: " +tele );
 
 
 
